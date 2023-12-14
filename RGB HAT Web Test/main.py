f7b10d97ec42b4d4b597d88b2705788ae7c26a0a
Daniel Williams <96486477+strongestgeek@users.noreply.github.com>
2023-12-14 11:52:11 +0000

    Update main.py
    Small tidy up of comments

--- RGB HAT Web Test/main.py
+++ RGB HAT Web Test/main.py
@@ -14,8 +14,8 @@ print("IP = ", ip)
 LED_COUNT      = 32      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 20     # Set to 0 for darkest and 255 for brightest
+LED_DMA        = 10      # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 20      # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 
 # Create NeoPixel object with appropriate configuration.
